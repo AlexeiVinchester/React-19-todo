7fbcf87006256c8c208bc25c102d89c1fd710358
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-02-27 13:40:37 +0300

    React-19-todo/master: added transitions for refetching of data

--- src/pages/users/ui/CreateUserForm.tsx
+++ src/pages/users/ui/CreateUserForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { createUser } from "../../../shared/api";
 
 type TCreateUserFormProps = {
@@ -7,26 +7,37 @@ type TCreateUserFormProps = {
 
 export const CreateUserForm = ({ refetchUsers }: TCreateUserFormProps) => {
   const [email, setEmail] = useState('');
+
+  const [isPending, startTransition] = useTransition();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await createUser({ email, id: crypto.randomUUID() });
-    refetchUsers();
-    setEmail(email);
-  }
+    startTransition(async () => {
+      await createUser({ email, id: crypto.randomUUID() });
+
+      startTransition(() => {
+        refetchUsers();
+        setEmail("");
+      })
+
+    });
+  };
 
   return (
     <form className="flex gap-2" onSubmit={handleSubmit}>
       <input
         type="email"
-        className="border p-2 m-2 rounded"
+        className="border p-2 m-2 rounded disabled:bg-gray-400"
+        disabled={isPending}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <button
         type="submit"
-        className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        disabled={isPending}
+        className="disabled:bg-gray-400 m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        Create user
+        Add
       </button>
     </form>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  server: {
+    watch: {
+      ignored: ['**/db.json'], // ✅ Vite не будет следить за изменениями в db.json
+    },
+  },
+});
