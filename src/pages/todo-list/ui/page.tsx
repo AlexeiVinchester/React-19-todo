ff7962c79a35f27f379ac1e856ea96e846c83ac9
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-03-06 14:45:46 +0300

    React-19-todo/master: created refetch and drill it to create form

--- src/pages/todo-list/ui/page.tsx
+++ src/pages/todo-list/ui/page.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useMemo, useState } from "react"
+import { startTransition, Suspense, useMemo, useState } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import { CreateTaskForm } from "./CreateTaskForm"
 import { TasksList, } from "./TasksList"
@@ -6,9 +6,15 @@ import { useParams } from "react-router"
 import { fetchTasks } from "../../../shared/tasksApi"
 
 export const TodolistPage = () => {
-  const { userId } = useParams();
+  const { userId = '' } = useParams();
 
-  const [paginatedTasksPromise, setPaginatedTasksPromise] = useState(() => fetchTasks({ filters: { userId } }));
+  const [paginatedTasksPromise, setPaginatedTasksPromise] = useState(
+    () => fetchTasks({ filters: { userId } })
+  );
+
+  const refetchTasks = () => {
+    startTransition(() => setPaginatedTasksPromise(fetchTasks({ filters: { userId } })))
+  };
 
   const tasksPromise = useMemo(
     () => paginatedTasksPromise.then((res) => res.data),
@@ -18,7 +24,7 @@ export const TodolistPage = () => {
   return (
     <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline mb-10">{userId}</h1>
-      <CreateTaskForm />
+      <CreateTaskForm refetchTasks={refetchTasks} userId={userId} />
       <ErrorBoundary fallbackRender={(e) => <div className="text-red-500">Something goes wrong! {JSON.stringify(e)}</div>}>
         <Suspense fallback={<p>Loading...</p>}>
           <TasksList tasksPromise={tasksPromise} />
