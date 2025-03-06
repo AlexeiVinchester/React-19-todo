40cc5f993b6ab98c084cfae8921138eb2a8d0cf6
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-03-06 14:29:22 +0300

    React-19-todo/master: added useActionState in createTaskForm

--- src/pages/todo-list/ui/CreateTaskForm.tsx
+++ src/pages/todo-list/ui/CreateTaskForm.tsx
@@ -1,20 +1,35 @@
-// type TCreateUserFormProps = {
-// }
+import { useActionState } from "react";
+import { createTaskActionWrapper } from "../lib/actions";
 
-export const CreateTaskForm = () => {
+type TCreateUserFormProps = {
+  refetchTasks: () => void;
+  userId: string;
+}
+
+export const CreateTaskForm = ({ refetchTasks, userId }: TCreateUserFormProps) => {
+
+  const [state, dispatch, isPending] = useActionState(
+    createTaskActionWrapper({ refetchTasks, userId }),
+    { title: '' }
+  );
+
   return (
-    <form className="flex gap-2" >
+    <form className="flex gap-2" action={dispatch}>
       <input
-
         className="border p-2 m-2 rounded disabled:bg-gray-400"
+        disabled={isPending}
+        name="title"
+        type="text"
+        defaultValue={state.title}
       />
       <button
         type="submit"
-
+        disabled={isPending}
         className="disabled:bg-gray-400 m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Add
       </button>
+      {state.error && <div className="text-red-500">{state.error}</div>}
     </form>
   );
 }
